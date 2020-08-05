1a25cdde3d4f6ccd4aca3953f4d5d69d469eed19
dmz <1055907692@qq.com>
2020-08-05 17:43:23 +0800

    MSBuildScript

--- MSBuildVisualStudio2019.bat
+++ MSBuildVisualStudio2019.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
-mkdir build
+if not exist build ( mkdir build )
 cd build
 rem Generate VS2019 project.
 cmake -G "Visual Studio 16 2019" -A "x64" -B build "-DCMAKE_TOOLCHAIN_FILE=F:/vcpkg/scripts/buildsystems/vcpkg.cmake" ..
