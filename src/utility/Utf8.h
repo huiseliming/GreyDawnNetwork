56850bd04b78d3452cdbdc6b1d1da196a0584797
HuiSeLiMing <1055907692@qq.com>
2020-08-12 14:12:12 +0800

    Unuse headeronly library to speed up compilation

--- src/utility/Utf8.h
+++ src/utility/Utf8.h
@@ -13,9 +13,10 @@ namespace GreyDawn
             return std::vector<uint32_t>(ascii.begin(),ascii.end());
         }
 
-        static std::vector<uint8_t> Encode(const std::string& )
+        static std::vector<uint8_t> Encode(std::vector<uint32_t> unicode)
         {
-
+            std::vector<uint8_t> utf8_encode;
+            return utf8_encode;
         }
     };
 }
