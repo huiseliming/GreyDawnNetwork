301e25592d1d73d4cbef41138baa0fe78ecb1193
dmz <1055907692@qq.com>
2020-08-13 17:19:12 +0800

    impl Utf8 decode

--- src/utility/Utf8.h
+++ src/utility/Utf8.h
@@ -16,14 +16,15 @@ namespace GreyDawn
         Utf8& operator=(Utf8&&) = delete;
     public:
         using UnicodeCodePoint = uint32_t;
-        inline static const std::array< uint8_t , 3> kUtf8EncodeReplacementCharacter = { 0xEF, 0xBF, 0xBD};
+        inline static const std::array< uint8_t , 3> kUtf8BytesReplacementCharacter = { 0xEF, 0xBF, 0xBD};
+        inline static const UnicodeCodePoint kUnicodeReplacementCharacter = 0xFFFD;
     public:
         static std::vector<UnicodeCodePoint> AsciiToUnicode(const std::string& ascii)
         {
             return std::vector<uint32_t>(ascii.begin(),ascii.end());
         }
 
-        static std::vector<uint8_t> Encode(std::vector<UnicodeCodePoint> code_points)
+        static std::vector<uint8_t> Encode(const std::vector<UnicodeCodePoint> code_points)
         {
             std::vector<uint8_t> utf8_bytes;
             for(auto code_point : code_points)
@@ -35,7 +36,7 @@ namespace GreyDawn
                     utf8_bytes.push_back(uint8_t((code_point & 0x3F) + 0x80));
                 } else if (code_point < 0x10000) {
                     if ((code_point >= UNUSE_CODE_POINT_BEGIN) && (code_point <= UNUSE_CODE_POINT_END)) { // Unicode在范围D800-DFFF中不存在任何字符
-                        utf8_bytes.insert(utf8_bytes.end(), kUtf8EncodeReplacementCharacter.begin(), kUtf8EncodeReplacementCharacter.end());
+                        utf8_bytes.insert(utf8_bytes.end(), kUtf8BytesReplacementCharacter.begin(), kUtf8BytesReplacementCharacter.end());
                     } else {
                         utf8_bytes.push_back(uint8_t(((code_point >> 12) & 0x0F) + 0xE0));
                         utf8_bytes.push_back(uint8_t(((code_point >> 6) & 0x3F) + 0x80));
@@ -48,7 +49,7 @@ namespace GreyDawn
                     utf8_bytes.push_back(uint8_t(((code_point >> 6) & 0x3F) + 0x80));
                     utf8_bytes.push_back(uint8_t((code_point & 0x3F) + 0x80));
                 } else {
-                    utf8_bytes.insert(utf8_bytes.end(), kUtf8EncodeReplacementCharacter.begin(), kUtf8EncodeReplacementCharacter.end());
+                    utf8_bytes.insert(utf8_bytes.end(), kUtf8BytesReplacementCharacter.begin(), kUtf8BytesReplacementCharacter.end());
                 }
 #else
                 } else if (code_point < 0x2000000) {
@@ -57,7 +58,7 @@ namespace GreyDawn
                     utf8_bytes.push_back(uint8_t(((code_point >> 6) & 0x3F) + 0x80));
                     utf8_bytes.push_back(uint8_t((code_point & 0x3F) + 0x80));
                 } else if (code_point < 0x40000000) {
-                    utf8_bytes.insert(utf8_bytes.end(), kUtf8EncodeReplacementCharacter.begin(), kUtf8EncodeReplacementCharacter.end());
+                    utf8_bytes.insert(utf8_bytes.end(), kUtf8BytesReplacementCharacter.begin(), kUtf8BytesReplacementCharacter.end());
 
                     utf8_bytes.push_back(uint8_t(((code_point >> 24) & 0x03) + 0xF8));
                     utf8_bytes.push_back(uint8_t(((code_point >> 18) & 0x3F) + 0x80));
@@ -65,7 +66,7 @@ namespace GreyDawn
                     utf8_bytes.push_back(uint8_t(((code_point >> 6) & 0x3F) + 0x80));
                     utf8_bytes.push_back(uint8_t((code_point & 0x3F) + 0x80));
                 } else {
-                    utf8_bytes.insert(utf8_bytes.end(), kUtf8EncodeReplacementCharacter.begin(), kUtf8EncodeReplacementCharacter.end());
+                    utf8_bytes.insert(utf8_bytes.end(), kUtf8BytesReplacementCharacter.begin(), kUtf8BytesReplacementCharacter.end());
                     utf8_bytes.push_back(uint8_t(((code_point >> 30) & 0x01) + 0xFC));
                     utf8_bytes.push_back(uint8_t(((code_point >> 24) & 0x3F) + 0x80));
                     utf8_bytes.push_back(uint8_t(((code_point >> 18) & 0x3F) + 0x80));
@@ -76,6 +77,49 @@ namespace GreyDawn
 #endif
             }
             return utf8_bytes;
+        }
+
+        static std::vector<UnicodeCodePoint> Decode(const std::string& utf8_string) 
+        {
+            std::vector<UnicodeCodePoint> unicode_vector;
+
+            return unicode_vector;
+        }
+        
+        static std::vector<UnicodeCodePoint> Decode(const std::vector<uint8_t>& utf8_bytes)
+        {
+            std::vector<UnicodeCodePoint> unicode_output;
+            UnicodeCodePoint unicode_code_point = 0;
+            size_t remaining_bytes = 0;
+            for (auto utf8_byte : utf8_bytes)
+            {
+                if (remaining_bytes == 0) {
+                    if ((utf8_byte & 0x80) == 0) {
+                        unicode_output.push_back(utf8_byte);
+                    } else if ((utf8_byte & 0xE0) == 0xC0) {
+                        remaining_bytes = 1;
+                        unicode_code_point += utf8_byte & 0x1F;
+                    } else if ((utf8_byte & 0xF0) == 0xE0) {
+                        remaining_bytes = 2;
+                        unicode_code_point += utf8_byte & 0xF;
+                    } else if ((utf8_byte & 0xF8) == 0xF0) {
+                        remaining_bytes = 3;
+                        unicode_code_point += utf8_byte & 0x7;
+                    } else{
+                        unicode_output.push_back(kUnicodeReplacementCharacter);
+                        unicode_code_point = 0;
+                    }
+                } else {
+                    unicode_code_point <<= 6;
+                    unicode_code_point += (utf8_byte & 0x3F);
+                    remaining_bytes--;
+                    if (remaining_bytes == 0) {
+                        unicode_output.push_back(unicode_code_point);
+                        unicode_code_point = 0;
+                    }
+                }
+            }
+            return unicode_output;
         }
     };
 }
--- tests/src/Utf8Test.cpp
+++ tests/src/Utf8Test.cpp
@@ -41,18 +41,59 @@ TEST(tests, EncodeChinese)
 
 TEST(tests, Symbols)
 {
-    //A≢Α.
-    const std::vector<uint8_t> expected_encoding{ 0x41, 0xE2 , 0x89, 0xA2 , 0xCE, 0x91, 0x2E };
-    const auto actual_encoding = Utf8::Encode({ 0x0041, 0x2262, 0x0391, 0x002E });
-    ASSERT_EQ(expected_encoding, actual_encoding);
+    {
+        //A≢Α.
+        const std::vector<uint8_t> expected_encoding{ 0x41, 0xE2 , 0x89, 0xA2 , 0xCE, 0x91, 0x2E };
+        const auto actual_encoding = Utf8::Encode({ 0x0041, 0x2262, 0x0391, 0x002E });
+        ASSERT_EQ(expected_encoding, actual_encoding);
+    }
+    {
+        //¢
+        const std::vector<uint8_t> expected_encoding{ 0xC2, 0xA2 };
+        const auto actual_encoding = Utf8::Encode({ 0xA2 });
+        ASSERT_EQ(expected_encoding, actual_encoding);
+    }
 }
 
 TEST(tests, InvalidCodePoint)
 {
-    const std::vector<uint8_t> expected_encoding(Utf8::kUtf8EncodeReplacementCharacter.begin(), Utf8::kUtf8EncodeReplacementCharacter.end());
+    const std::vector<uint8_t> expected_encoding(Utf8::kUtf8BytesReplacementCharacter.begin(), Utf8::kUtf8BytesReplacementCharacter.end());
+    ASSERT_EQ((std::vector<uint8_t>{0xED, 0x9F, 0xBF}), Utf8::Encode({ 0xD7FF }));
     ASSERT_EQ(expected_encoding, Utf8::Encode({ 0xD801 }));
     ASSERT_EQ(expected_encoding, Utf8::Encode({ 0xDFFE }));
+    ASSERT_EQ((std::vector<uint8_t>{0xEE, 0x80, 0x80}), Utf8::Encode({ 0xE000 }));
     ASSERT_EQ(expected_encoding, Utf8::Encode({ 0x2000000 }));
     ASSERT_EQ(expected_encoding, Utf8::Encode({ 0xFFFFFFFF }));
 }
+
+TEST(tests,DecodeValidSequences) 
+{
+    //struct TestVector{
+    //    std::string encoding;
+    //    std::vector<Utf8::UnicodeCodePoint> expected_encoding;
+    //};
+    //const std::vector<TestVector> test_vectors{
+    //    { "不", { 0x233B4 } },
+    //    { "中文", { 0x4E2D,0x6587 } },
+    //    { "A≢Α.", { 0x0041, 0x2262, 0x0391, 0x002E } },
+    //    { "¢", { 0xA2 } }
+    //};
+    //for (const auto& test_vector : test_vectors)
+    //{
+    //    const auto actual_encoding = Utf8::Decode(test_vector.encoding);
+    //    ASSERT_EQ(test_vector.expected_encoding, actual_encoding);
+    //}
+}
+
+TEST(tests,DecodeFromInputVector) 
+{
+    const auto actual_encoding = Utf8::Decode(std::vector<uint8_t>{0xE6, 0x97, 0xA5, 0xE6, 0X9C, 0xAC, 0xE8, 0xAA, 0x9E});
+    ASSERT_EQ((std::vector<Utf8::UnicodeCodePoint>{0x65E5, 0x672C, 0x8A9E}), actual_encoding);
+}
+
+
+
+
+
+
 
